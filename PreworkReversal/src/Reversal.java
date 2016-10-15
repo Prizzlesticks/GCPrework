332c5cb246a5575af491aa3eb0fcc6db0411185d
Prizzlesticks <robert.prill@gmail.com>
2016-10-15 00:23:25 -0400

    Add files via upload

--- PreworkReversal/src/Reversal.java
+++ PreworkReversal/src/Reversal.java
@@ -5,7 +5,7 @@ public class Reversal {
 	public static void main(String[] args) {
 		
 		int value = 0;
-		int reverse = 0;
+		int newNumber = 0;
 		
 		// Create scanner object
 		Scanner input = new Scanner(System.in);
@@ -18,19 +18,19 @@ public class Reversal {
 		
 		//Reverse the number
 		
-		//Use a while loop to break down the input
+		//Use a while loop to break down the input - loop continues while not equal to 0
 		while(value != 0 )
 	{
 		//Multiply the reverse number to finish inversion
-		reverse = reverse * 10;
+		newNumber = newNumber * 10;
 		//Use Modulus to divide and get a remainder
-		reverse = reverse + value%10; 
+		newNumber = newNumber + value%10; 
 		//Use the value input and divide by 10 to obtain next number
 		value = value/10;
 	}
 	
 		// Output user Entry 
-		System.out.println("The reverse of your entry is " + reverse);
+		System.out.println("The reverse of your entry is " + newNumber);
 		
 		
 		// Found code to close memory leak
